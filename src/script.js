b8c1b643bf78bed7b803c473fddba6bf3a6b9fb2
rndmlmnt <74506322+rndmlmnt@users.noreply.github.com>
2020-11-16 01:40:58 +0100

    Initial commit

---
+++ src/script.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>,.</h1>
+<div>
+  yt
+  <a href="https://youtu.be/bG2BmmYr9NQ" target="_blank" rel="noopener noreferrer">oo</a>.
+</div>
+`;
